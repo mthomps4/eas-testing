fb7fd5bf9426d6ed4fb0942b17e6a0deabad66db
Matt Thompson <matt@echobind.com>
2022-03-29 16:47:33 -0400

    Home and EAS

--- App.js
+++ App.js
@@ -1,12 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, Image, Platform } from "react-native";
+import { useAssets } from "expo-asset";
+import Constants from "expo-constants";
 
 export default function App() {
+  const [assets, error] = useAssets(require("./assets/baby_groot.png"));
+
+  const identifier =
+    Platform.OS === "ios"
+      ? Constants.manifest?.ios?.bundleIdentifier
+      : Constants.manifest?.android.versionCode;
+
+  if (!assets && !error) {
+    return (
+      <View>
+        <Text>Loading</Text>
+      </View>
+    );
+  }
+  const [logo] = assets;
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Image source={logo} style={styles.logo} />
+      <Text style={styles.header}>{Constants.manifest.name}</Text>
+      <Text style={styles.subheader}>{Constants.manifest.version}</Text>
+      <Text style={styles.text}>{identifier || "local"}</Text>
     </View>
   );
 }
@@ -14,8 +35,25 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  header: {
+    fontSize: 30,
+    fontWeight: "bold",
+    color: "teal",
+  },
+  subheader: {
+    fontSize: 24,
+    color: "teal",
+  },
+  text: {
+    fontSize: 14,
+    color: "teal",
+  },
+  logo: {
+    width: 200,
+    height: 200,
   },
 });
