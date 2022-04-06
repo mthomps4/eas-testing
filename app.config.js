54f8c6da50a5f2ff6d31ea67d130f1b0347f0b67
Matt Thompson <matt@echobind.com>
2022-04-05 23:29:56 -0400

    Config based off Profile ENV from eas

--- app.config.js
+++ app.config.js
@@ -1,31 +1,57 @@
 // https://docs.expo.dev/build-reference/variables/
-const buildId = process.env.EAS_BUILD_ID || "local";
-const profile = process.env.EAS_BUILD_PROFILE || "local";
-const gitHash = process.env.EAS_BUILD_COMMIT_HASH || "local";
-const isCI = process.env.CI; // 1 or 0
+// https://docs.expo.dev/build-reference/variables/#built-in-environment-variables
+
+// They are not set when evaluating app.config locally
+
+// const buildId = process.env.EAS_BUILD_ID || "local";
+// const profile = process.env.EAS_BUILD_PROFILE || "local";
+// const gitHash = process.env.EAS_BUILD_COMMIT_HASH || "local";
+// const isCI = process.env.CI; // 1 or 0
+// console.log("\n\n\n\n\n");
+// console.log({ buildId, profile, gitHash, isCI });
+// console.log("\n\n\n\n\n");
+
+// When running `eas build -p ios --profile alpha :thinking:
+// I wonder if this is different from CI?
+
+// {
+//   buildId: 'local',
+//   profile: 'local',
+//   gitHash: 'local',
+//   isCI: undefined,
+//   APP_ENV: 'alpha'
+// }
+
+// https://docs.expo.dev/build-reference/variables/#can-i-share-environment-variables-defined-in
+// "Use a single environment variable to toggle configuration."
 
 // Custom Set in eas.json
-const APP_ENV = process.env.APP_ENV || "nope";
+const profile = process.env.profile;
 
-console.log("\n\n\n\n\n");
-console.log({ buildId, profile, gitHash, isCI, APP_ENV });
-console.log("\n\n\n\n\n");
+let config = {};
 
-export default {
-  expo: {
-    name: `baby-groot-${profile}`,
-    slug: `baby-groot-${profile}`,
-    owner: "mthomps4",
-    version: "2.0.6",
-    assetBundlePatterns: ["**/*"],
-    extra: {
-      eas: {
-        projectId: "8482c23d-2aed-46f1-aacb-8efa56bddddc",
-        appEnv: APP_ENV,
+if (profile) {
+  const appName = `baby-groot-${profile}`;
+
+  config = {
+    expo: {
+      name: appName,
+      slug: appName,
+      owner: "mthomps4",
+      version: "2.0.6", // 2.0.6-7 ?? Chat w/ Dominic later.
+      assetBundlePatterns: ["**/*"],
+      extra: {
+        eas: {
+          projectId: "8482c23d-2aed-46f1-aacb-8efa56bddddc",
+          profile,
+          appName,
+        },
+      },
+      ios: {
+        buildNumber: "7", // GitHash or buildId Combo? Manual bump for now config.js cannot autoIncrement
       },
     },
-    ios: {
-      buildNumber: "5", // GitHash or buildId Combo?
-    },
-  },
-};
+  };
+}
+
+export default config;
