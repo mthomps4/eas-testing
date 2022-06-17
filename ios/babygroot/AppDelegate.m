050c084e019b43da84a2f6b6ee5abdadfd78daea
Matt Thompson <matt@echobind.com>
2022-06-17 13:56:28 -0400

    fresh versions package/pods

--- ios/babygroot/AppDelegate.m
+++ ios/babygroot/AppDelegate.m
@@ -61,6 +61,7 @@ static void InitializeFlipper(UIApplication *application) {
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+  [super application:application didFinishLaunchingWithOptions:launchOptions];
 
   return bridge;
  }
