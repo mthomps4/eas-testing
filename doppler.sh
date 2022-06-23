fa7afdb5d341d988cb18552ff90fb817fe9d595b
Matt Thompson <matt@echobind.com>
2022-06-23 11:16:44 -0400

    tweak script

--- doppler.sh
+++ doppler.sh
@@ -3,7 +3,7 @@
 # https://github.com/travis-ci/travis-build/blob/061cd364c38a07c8908b420b9dd134d86b537b01/lib/travis/build/bash/travis_setup_env.bash#L22-L35
 
 echo "uname"
-echo uname
+echo $uname
 echo "ostype"
 echo $OSTYPE
 
@@ -11,7 +11,6 @@ install_doppler_with_apt () {
   echo "installing doppler with apt"
   apt install -y curl gnupg
   (curl -Ls --tlsv1.2 --proto "=https" --retry 3 https://cli.doppler.com/install.sh || wget -t 3 -qO- https://cli.doppler.com/install.sh) | sudo sh
-
 }
 
 install_doppler_with_brew() {
@@ -20,14 +19,13 @@ install_doppler_with_brew() {
   brew install dopplerhq/cli/doppler
 }
 
-if uname == "Darwin" {
+if grep -qi "darwin" <<< $OSTYPE ; then
   install_doppler_with_brew()
-} else {
+else
   install_doppler_with_apt()
-}
+fi
 
-
-doppler run --token="$DOPPLER_TOKEN" -- true
+# doppler run --token="$DOPPLER_TOKEN" -- true
 
 # # Should pull from Expo Secrets
 # export HISTIGNORE='export DOPPLER_TOKEN*'
